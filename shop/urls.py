1d0cd3c6f8a90d370d81b0ed7ec75738671a6f57
Mursaliev-Ariet <d98631996@gmail.com>
2026-05-24 21:55:33 +0600

    shop3

--- shop/urls.py
+++ shop/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
 from . import views
 
-urlpatterns = []
+urlpatterns = [
+    path('clothes/', views.clothes_list, name='clothes_list'),
+]
--- shop/views.py
+++ shop/views.py
@@ -1,1 +1,8 @@
 from django.shortcuts import render
+
+from shop.models import Clothes
+
+
+def clothes_list(request):
+    clothe_list = Clothes.objects.all()
+    return render(request, 'Clothes/clothes_list.html',{"clothes_list" : clothe_list})
