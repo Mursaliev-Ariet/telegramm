8b8a48e6c1afb328acb43f172326674ae3ae859c
Mursaliev-Ariet <d98631996@gmail.com>
2026-05-24 21:10:37 +0600

    shop2

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Clothes(models.Model):
+    name = models.CharField(max_length=100)
+    text = models.TextField()
+    price = models.IntegerField()
+    photo = models.ImageField(upload_to='clothes')
+    def __str__(self):
+        return self.name
+    class Meta:
+        verbose_name = 'Одежда'
+        verbose_name_plural = 'Одежда'
