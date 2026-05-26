1f37d4aed8a997cfa3537f89aabeddcde0a09c37
Mursaliev-Ariet <d98631996@gmail.com>
2026-05-26 21:15:14 +0600

    shop9

--- config/urls.py
+++ config/urls.py
@@ -5,9 +5,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='404.html')),
+    path('', TemplateView.as_view(template_name='clothes_list.html')),
     path('admin/', admin.site.urls),
-    path('shop_list/', include('shop.urls')),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
