7deef18084a35590e231927f696d0eff5b273247
Mursaliev-Ariet <d98631996@gmail.com>
2026-05-28 16:48:48 +0600

    shop11

--- config/settings.py
+++ config/settings.py
@@ -124,6 +124,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 import os
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- config/urls.py
+++ config/urls.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic import TemplateView
 from django.conf import settings
 from django.conf.urls.static import static
+from shop.views import clothes_list
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='Clothes/clothes_list.html')),
+    path('', clothes_list, name='home'),
     path('admin/', admin.site.urls),
+    path('', include('shop.urls')),
 ]
+
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
